89a0a4854247c486bc7eadd83b8a62534c0e43ec
NagaSowmyaSri <chnsowmyasri@gmail.com>
2024-10-20 16:02:21 +0530

    Updates

---
+++ precache-manifest.9ead83b952e12a7fdce0baf89e493eba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63acbe2338f811e6ab3675a1670a1689",
+    "url": "/NagaSowmyaSri/Event/index.html"
+  },
+  {
+    "revision": "4b0ec947027470467736",
+    "url": "/NagaSowmyaSri/Event/static/css/main.51afd845.chunk.css"
+  },
+  {
+    "revision": "bbfd08548fca63293933",
+    "url": "/NagaSowmyaSri/Event/static/js/2.5dcab6ca.chunk.js"
+  },
+  {
+    "revision": "4b0ec947027470467736",
+    "url": "/NagaSowmyaSri/Event/static/js/main.019b4a78.chunk.js"
+  },
+  {
+    "revision": "6e67d258fe83adb282f2",
+    "url": "/NagaSowmyaSri/Event/static/js/runtime~main.e9418c20.js"
+  }
+]);
